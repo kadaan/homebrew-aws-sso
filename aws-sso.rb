f2012c606950488fc893e705b6f67129b6ed9829
Joel Baranick <joel.baranick@ensighten.com>
2018-06-28 00:14:02 -0700

    Update url

--- aws-sso.rb
+++ aws-sso.rb
@@ -2,7 +2,7 @@ class AwsSso < Formula
   desc "Tool to generate temporary AWS credentials using Okta SSO"
   homepage "https://github.com/kadaan/homebrew-aws-sso/homebrew-aws-sso.git"
 
-  url "https://github.com/kadaan/homebrew-aws-sso/releases/download/untagged-0dc703aa117780d899cf/aws-profile_1.4.0_mac.tbz2"
+  url "https://github.com/Ensighten/homebrew-formulae/releases/download/untagged-013ba2cb15c230473661/aws-profile_1.4.0_mac.tbz2"
   sha256 "19467c1d851348c889cf4230704102b9523d30918ea8c24671f5d7597880ad3e"
   version "1.4.0"
 
