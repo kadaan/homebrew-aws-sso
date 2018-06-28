6b264767d0295788ddd4746eb50ac2b361a1060b
Joel Baranick <joel.baranick@ensighten.com>
2018-06-27 17:40:11 -0700

    Update hash

--- aws-sso.rb
+++ aws-sso.rb
@@ -3,7 +3,7 @@ class AwsSso < Formula
   homepage "https://github.com/kadaan/homebrew-aws-sso/homebrew-aws-sso.git"
 
   url "https://github.com/kadaan/homebrew-aws-sso/releases/download/untagged-0dc703aa117780d899cf/aws-profile_1.4.0_mac.tbz2"
-  sha256 "a47453b15feb1c5ce4d3d6ad062d331b29dfdb38f4964294b4571b4d794aeddc"
+  sha256 "447b4755890d6e435d08ecb354a50c1f0f0b0d7f5765315d8aa7048ee26699fe"
   version "1.4.0"
 
   bottle :unneeded
